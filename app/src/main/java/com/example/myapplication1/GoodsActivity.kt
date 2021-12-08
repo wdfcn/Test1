c29a9dcf4052c103fb72c2634a66284e6e0a1a50
Liu78781178 <1518388355@qq.com>
2021-12-08 11:02:26 +0800

    update 1

--- app/src/main/java/com/example/myapplication1/GoodsActivity.kt
+++ app/src/main/java/com/example/myapplication1/GoodsActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ms.square.android.expandabletextview.ExpandableTextView
 import kotlinx.android.synthetic.main.activity_goods.*
 
 class GoodsActivity : AppCompatActivity() {
@@ -10,6 +11,8 @@ class GoodsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_goods)
         val text = intent.getStringExtra("id")
         text_id.text = text
+        val expTv1 : ExpandableTextView = findViewById(R.id.expand_text_view)
+        expTv1.setText("qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq")
     }
 
 }
