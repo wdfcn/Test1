e6c5b5b95c24c7463ee76ae11f15f11c556bc614
wdfcn <1518388355@qq.com>
2021-12-09 15:15:02 +0800

    computer0 12.9.0

---
+++ app/src/main/java/com/example/myapplication1/recommand.kt
@@ -0,0 +1,39 @@
+package com.example.myapplication1
+
+class recommand{
+    fun recommand(id:Int): Pair<String, String> {
+        var boughtlist = arrayListOf<Int>()
+        var tag = arrayListOf<String>()
+        var taglist = arrayListOf<String>()
+        var tagnum = arrayListOf<Int>()
+        var first_tag : String = ""
+        var second_tag : String = ""
+        var max:Int = 0
+        for (item in boughtlist){
+            tag.add("")
+            for (item in tag)
+            {
+                if(taglist.contains(item)==false)
+                {
+                    taglist.add(item)
+                    tagnum.add(0)
+                }
+                else
+                {
+                    val num = taglist.indexOf(item)
+                    tagnum[num]++
+                }
+            }
+        }
+        for (i in tagnum.indices)
+        {
+            if(tagnum[i]>max)
+            {
+                max = tagnum[i]
+                second_tag = first_tag
+                first_tag = taglist[i]
+            }
+        }
+        return Pair(first_tag, second_tag)
+    }
+}
